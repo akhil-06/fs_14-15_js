e191208d29b29982f8fa32ced2e8248b03817998
akhil-06 <svam1008@gmail.com>
2023-12-22 21:27:34 +0530

    day2

---
+++ day2/script.js
@@ -0,0 +1,138 @@
+// console.log("Hello World!!!");
+//  (int)5.67;
+
+//data type conversion -> implicit (coercion), explicit(type casting)
+
+//implicit type convertion
+// let num = 10 - "hello";
+// console.log( num);
+
+// let string = " ";
+// let num = 50;
+// let boolean = true;
+// console.log(!string);
+// console.log(string + boolean);
+
+// console.log(20 == "20");
+
+
+
+//Explicit type conversion (type casting)
+// let num = 10;
+// let stringNum = String(num);
+// let bollNum = Boolean(num);
+// let bigNum = BigInt(num);
+// console.log(bigNum);
+// console.log(stringNum);
+// console.log(bollNum);
+
+//swap values without third variable
+// let a = 4;
+// let b = 8;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a , b);
+
+// - **Swap Values:** Implement a function that swaps the values of two variables without using a temporary variable. Observe implicit type coercion.
+
+// - **Numeric Operation with Strings:** Write a program that takes two variables containing numeric strings and performs addition or subtraction on them.
+
+// - **Concatenate Strings and Booleans:** Create variables containing strings and booleans and use them in concatenation operations to observe type coercion.
+
+
+// selection and control statements
+// if, if-else, nested ifelse, swicth
+//example:- cookies, amazon discount, 
+
+
+// let price = 700;
+// if (price > 1000){
+//     let discount = price * 0.10;
+//     console.log(discount);
+//     console.log(price - discount);
+// }else if(price > 500){
+//     let discount = price * 0.05;
+//     console.log(discount);
+//     console.log(price - discount);
+// }else{
+//     // let discount = price * 0.10;
+//     console.log("no discount");
+//     console.log(price);
+// }
+
+// if
+// if
+// if
+
+//switch case
+// let shipping = '';
+// switch(shipping) {
+//     case "Standard":
+//         console.log("within 5-7 working days");
+//         break;
+//     case "Express":
+//         console.log("within 2-3 working days");
+//         break;
+//     case "One-Day":
+//         console.log("within 1 working days");
+//         break;
+//     default:
+//         console.log("No Shipping");
+// }
+
+
+
+// llops
+
+//for loop
+// let string = "";
+// for(let i = 1; i<=10 ;i++){
+//     // console.log(i);
+//     string = string + i + " ";
+// }
+// console.log(string);
+
+//while loop
+// let i =120;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+//do-while
+// let i = 120;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10);
+
+
+// *****
+// *****
+// *****
+// *****
+// *****
+//rows, cols, spaces, num
+
+// let num = 15;
+
+// for(let i=0;i<num;i++){    //rows
+//     let string = "";
+//     for(let j=0;j<num;j++){  //cols
+//         string = string + "*";
+//     }
+//     console.log(string);
+// }
+
+
+
+//functions
+
+function sum(a,b){
+    // console.log(a+b);
+    return a+b;
+}
+
+let asn = sum(2,4);
+console.log(asn);
