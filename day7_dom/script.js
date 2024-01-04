354d3f31a9f5dfd76c848e537654f47656e6ea94
akhil-06 <svam1008@gmail.com>
2024-01-04 21:49:44 +0530

    dom started

---
+++ day7_dom/script.js
@@ -0,0 +1,28 @@
+console.log("Hello World!!!!");
+
+let h1 = document.getElementById("heading1");
+let div = document.getElementById("div1");
+console.log(div);
+console.log(div.innerText);
+console.log(div.innerHTML);
+// // console.log(h1);
+// let paras = document.getElementsByClassName("para");
+// console.log(paras);
+
+// for(let i=0;i<paras.length;i++){
+//     paras[i].style.backgroundColor = "red";
+// }
+
+
+// h1.style.backgroundColor = "green";
+
+// let tags = document.getElementsByTagName("p");
+// console.log(tags);
+
+// let para = document.querySelector("#heading1");
+// para.setAttribute("class", "head");
+// console.log(para);
+// console.log(para);
+
+// let myArray = Array.from(para);
+// console.log(myArray);
